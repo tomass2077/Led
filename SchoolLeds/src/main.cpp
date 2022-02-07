d6a20a3392854d60c5a99864fb84ebc7715613e3
tomass2077 <79580592+tomass2077@users.noreply.github.com>
2022-02-07 18:13:17 +0200

    Initial upload

---
+++ SchoolLeds/src/main.cpp
@@ -0,0 +1,275 @@
+#include <Arduino.h>
+
+#include <FastLED.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <EEPROM.h>
+#include <ArduinoJson.h>
+#include <WiFiUdp.h>
+#define UDP_PORT 4210
+#define DATA_PIN 2
+#define COLOR_ORDER GRB
+#define NUM_LEDS 30
+#define LED_TYPE WS2812
+#define BRIGHTNESS 64
+CRGB leds[NUM_LEDS];
+
+WiFiUDP UDP;
+String SendHTML(uint8_t P);
+void UThere();
+void HandleCol();
+String ssid = "";
+String pass = "";
+
+DynamicJsonDocument doc(512);
+String IP = "@@@@";
+ESP8266WebServer server(80);
+uint8_t program = 1;
+uint8_t ID = 255;
+String getValue(String data, char separator, int index)
+{
+	int found = 0;
+	int strIndex[] = {0, -1};
+	int maxIndex = data.length() - 1;
+
+	for (int i = 0; i <= maxIndex && found <= index; i++)
+	{
+		if (data.charAt(i) == separator || i == maxIndex)
+		{
+			found++;
+			strIndex[0] = strIndex[1] + 1;
+			strIndex[1] = (i == maxIndex) ? i + 1 : i;
+		}
+	}
+	return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+//Writes Mains IP
+void WriteIP(String ip)
+{
+	int addr = 1;
+
+	String string1 = getValue(ip, '.', 0);
+	String string2 = getValue(ip, '.', 1);
+	String string3 = getValue(ip, '.', 2);
+	String string4 = getValue(ip, '.', 3);
+
+	uint8_t num1 = string1.toInt();
+	uint8_t num2 = string2.toInt();
+	uint8_t num3 = string3.toInt();
+	uint8_t num4 = string4.toInt();
+
+	EEPROM.write(addr, num1);
+	addr++;
+	EEPROM.write(addr, num2);
+	addr++;
+	EEPROM.write(addr, num3);
+	addr++;
+	EEPROM.write(addr, num4);
+	IP = (String)num1 + (String)num2 + (String)num3 + (String)num4;
+	EEPROM.commit();
+}
+//gets Mains IP saved
+String GetIP()
+{
+	int addr = 1;
+
+	String string;
+	uint8_t num1=0;
+	uint8_t num2=0;
+	uint8_t num3=0;
+	uint8_t num4=0;
+
+	num1 = EEPROM.get(addr, num1);
+	addr++;
+	num2 = EEPROM.get(addr, num2);
+	addr++;
+	num3 = EEPROM.get(addr, num3);
+	addr++;
+	num4 = EEPROM.get(addr, num4);
+	string = (String)num1 + "." + (String)num2 + "." + (String)num3 + "." + (String)num4;
+	return (string);
+}
+
+//Writes  ID
+void WriteID(uint8_t ip)
+{
+	EEPROM.write(0, ip);
+
+	EEPROM.commit();
+}
+//gets ID saved
+uint8_t GetID()
+{
+	uint8_t num1=0;
+
+	num1 = EEPROM.get(0, num1);
+	return (num1);
+}
+void CheckInputs(){
+	String op = Serial.readString();
+		if (op[0] == 'i')
+		{
+			if (op[1] == 'p')
+			{
+				op.replace("ip", "");
+				WriteIP(op);
+				IP = GetIP();
+				Serial.print("Ip set to:" + op);
+			}
+			if (op[1] == 'd')
+			{
+				op.replace("id", "");
+				WriteID(op.toInt());
+				ID = GetID();
+				Serial.print("ID set to:" + op);
+			}
+		}
+}
+int blynk = 100;
+int blynkMilis = 100;
+bool blinky = false;
+void setup()
+{
+	//Led strip setup
+	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+	FastLED.setCorrection(TypicalLEDStrip);
+	ID = GetID();
+	//Just blinkeys
+	for (int i = 0; i < 10; i++)
+	{
+		
+		leds[0] = CRGB::White;
+		FastLED.show();
+		CheckInputs();
+		delay(20);
+		leds[0] = CRGB::Black;
+		FastLED.show();
+		delay(500);
+	}
+
+	FastLED.clear();
+	//Begins Storage
+	EEPROM.begin(512);
+	IP = GetIP();
+	WiFi.hostname("esp01");
+	Serial.begin(115200);
+	//Connect to wifi
+	WiFi.begin(ssid, pass);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		leds[0] = CRGB::Red;
+		FastLED.show();
+		delay(100);
+		leds[0] = CRGB::Black;
+		FastLED.show();
+		delay(100);
+	}
+	Serial.println("");
+	Serial.println("Got main IP: " + IP);
+	Serial.println("Got Mac: " + WiFi.macAddress());
+	Serial.println("Got IP: " + WiFi.localIP().toString());
+	//Server propeties
+	server.on("/espinator/SetCol", HandleCol);
+	server.on("/espinator/UThere", UThere);
+	server.begin();
+
+	// Get id and send ip from Mac
+	WiFiClient client;
+	HTTPClient http;
+	String serverPath = "http://" + IP + "/espinator/MyData?Mac=" + WiFi.macAddress()+"&ID=" + ID + "&IP=" + WiFi.localIP().toString();
+	http.setTimeout(200);
+	http.begin(client, serverPath.c_str());
+	int httpCode = http.GET();
+
+	bool ledy = false;
+	bool searching = true;
+	//search for main server
+	while (searching)
+	{
+		CheckInputs();
+		if (httpCode != -1)
+		{
+			deserializeJson(doc, http.getString());
+			//ID = doc["id"].as<int>();
+			searching = false;
+			Serial.println("Found server" + (String)httpCode);
+		}
+		else
+		{
+			http.begin(client, serverPath.c_str());
+			httpCode = http.GET();
+			if (ledy)
+				leds[0] = CRGB::Green;
+			else
+				leds[0] = CRGB::Black;
+			FastLED.show();
+			ledy = !ledy;
+			Serial.println("can't find server" + (String)httpCode);
+		}
+		delay(10);
+	}
+
+	leds[0] = CRGB::Black;
+	FastLED.show();
+	Serial.println("Got ID: " + (String)ID);
+	blinky = false;
+	//digitalWrite(2, true);
+	UDP.begin(UDP_PORT);
+
+	for (int i = 0; i < 2; i++)
+	{
+		leds[0] = CRGB::Yellow;
+		FastLED.show();
+		delay(100);
+		leds[0] = CRGB::Black;
+		FastLED.show();
+		delay(100);
+	}
+}
+uint8_t packet[32];
+//int col = 0;
+void loop()
+{
+	server.handleClient();
+	int packetSize = UDP.parsePacket();
+	if (packetSize)
+	{
+		int len = UDP.read(packet, 32);
+		//bool led = true;
+		if (len > 0)
+		{
+			packet[len] = '\0';
+		}
+
+		//int Frame = packet[1] + (255 * packet[2]);
+		int NewColR = packet[0];
+		int NewColG = packet[1];
+		int NewColB = packet[2];
+		FastLED.showColor(CRGB(NewColR, NewColG, NewColB));
+		Serial.println(String(NewColR)+" "+String(NewColG)+" "+String(NewColB));
+	}
+	delay(1);
+}
+
+//Server Asks
+void UThere()
+{
+	server.send(200, "application/json", "{ \"status\":\"ok\"}");
+	Serial.println("U There");
+}
+//Sent Col
+void HandleCol()
+{
+	if (server.arg("Col") != "")
+	{
+		int r, g, b;
+		char const *hexColor = server.arg("Col").c_str();
+		std::sscanf(hexColor, "#%02x%02x%02x", &r, &g, &b);
+		FastLED.showColor(CRGB(r, g, b));
+
+		server.send(200, "application/json", "{ \"status\":\"ok\"}");
+		blinky = false;
+	}
+}
---
+++ SchoolLedsMain/src/main.cpp
@@ -0,0 +1,500 @@
+#include <Arduino.h>
+
+/*
+ Name:		SchoolLedMain.ino
+ Created:	1/14/2022 12:20:31 PM
+ Author:	Tomass
+*/
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <SPI.h>
+#include <SD.h>
+#include <WString.h>
+#include <U8g2lib.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <WiFiUdp.h>
+#include <stdio.h>
+#include <cstring>
+#define UDP_PORT 4210
+WiFiUDP UDP;
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+void HandleMyData();
+ESP8266WebServer server(80);
+String ssid = "";
+String pass = "";
+
+const size_t capacit = 512 * 8;
+DynamicJsonDocument doc(capacit);
+File SdCard;
+void Blank(){
+	for (int i = 0; i < 27; i++)
+	{
+		IPAddress pps;
+		pps.fromString(doc["devices"][i]["IP"].as<String>());
+
+		for (int j = 0; j < 64; j++)
+		{
+			UDP.beginPacket(pps, UDP_PORT);
+
+			//for (int jj = 0; jj < 16; jj++)
+			UDP.write(uint8_t(0));
+			UDP.write(uint8_t(0));
+			UDP.write(uint8_t(0));
+			UDP.write(uint8_t(0));
+			UDP.write(uint8_t(0));
+			UDP.endPacket();
+		}
+	}
+}
+void setup()
+{
+	u8g2.begin();
+	Serial.begin(115200);
+	WiFi.begin(ssid, pass);
+	WiFi.hostname("espMain");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+		delay(500);
+	}
+
+	//const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+	SD.begin(D8);
+	SdCard = SD.open("modules.json");
+	String pp = SdCard.readString();
+	//pp.trim();
+	Serial.println(pp);
+	Serial.println("Got IP: " + WiFi.localIP().toString());
+	//const char* ssid = pp;
+	DeserializationError err = deserializeJson(doc, pp);
+	Serial.println(err.c_str());
+	Serial.println(doc["devices"].size());
+	if (1)
+		for (int i = 0; i < 27; i++)
+		{
+			WiFiClient client;
+			HTTPClient http;
+			String serverPath = "http://" + doc["devices"][i]["IP"].as<String>() + "/espinator/UThere";
+			http.setTimeout(300);
+			http.begin(client, serverPath.c_str());
+			int httpCode = http.GET();
+			if (http.getString() != "")
+				doc["devices"][i]["found"] = true;
+			else
+				doc["devices"][i]["found"] = false;
+
+			u8g2.setFont(u8g2_font_profont12_tr);
+
+			if (!doc["devices"][i]["found"])
+				u8g2.setDrawColor(1);
+			else
+				u8g2.setDrawColor(1);
+			int offset = 25;
+			if (i < 3)
+				u8g2.setCursor(5 + offset * 0, 9 + 9 * (i - 0));
+			else if (i < 6)
+				u8g2.setCursor(5 + offset * 1, 9 + 9 * (i - 3));
+			else if (i < 9)
+				u8g2.setCursor(5 + offset * 2, 9 + 9 * (i - 6));
+			else if (i < 12)
+				u8g2.setCursor(5 + offset * 3, 9 + 9 * (i - 9));
+			else if (i < 15)
+				u8g2.setCursor(5 + offset * 0, 15 + 9 * (i - 9));
+			else if (i < 18)
+				u8g2.setCursor(5 + offset * 1, 15 + 9 * (i - 12));
+			else if (i < 21)
+				u8g2.setCursor(5 + offset * 2, 15 + 9 * (i - 15));
+			else if (i < 24)
+				u8g2.setCursor(5 + offset * 3, 15 + 9 * (i - 18));
+			else if (i < 27)
+				u8g2.setCursor(5 + offset * 4, 15 + 9 * (i - 21));
+			if (doc["devices"][i]["found"])
+			{
+				if (i < 10)
+					u8g2.print(".0" + (String)i + ".");
+				else
+					u8g2.print("." + (String)i + ".");
+			}
+			else
+			{
+				if (i < 10)
+					u8g2.print("?0" + (String)i + "?");
+				else
+					u8g2.print("?" + (String)i + "?");
+			}
+			u8g2.sendBuffer();
+		}
+		Blank();
+	SdCard.close();
+	server.on("/espinator/MyData", HandleMyData);
+	server.begin();
+	pinMode(0, INPUT_PULLUP);
+}
+bool Up = false;
+bool Down = false;
+bool UpRn = false;
+bool DownRn = false;
+bool butonRn = false;
+bool buton = false;
+bool butonOld = false;
+void handleInput()
+{
+	UpRn = false;
+	DownRn = false;
+	butonRn = false;
+	buton = digitalRead(0);
+	int a = analogRead(0);
+	if (buton != butonOld && buton == false)
+		butonRn = true;
+	if (a > 750)
+	{
+		if (!Up)
+			UpRn = true;
+		Up = true;
+		Down = false;
+	}
+	else if (a < 250)
+	{
+		if (!Down)
+			DownRn = true;
+		Up = false;
+		Down = true;
+	}
+	else
+	{
+		Up = false;
+		Down = false;
+	}
+
+	butonOld = buton;
+}
+void ConnectedOnes()
+{
+	while (true)
+	{
+		u8g2.clearBuffer();
+		server.handleClient();
+		handleInput();
+		if (butonRn)
+		{
+			break;
+		}
+		for (int i = 0; i < 27; i++)
+		{
+			u8g2.setFont(u8g2_font_profont12_tr);
+			//u8g2.set
+			if (!doc["devices"][i]["found"])
+				u8g2.setDrawColor(1);
+			else
+				u8g2.setDrawColor(1);
+			int offset = 25;
+			if (i < 3)
+				u8g2.setCursor(5 + offset * 0, 9 + 9 * (i - 0));
+			else if (i < 6)
+				u8g2.setCursor(5 + offset * 1, 9 + 9 * (i - 3));
+			else if (i < 9)
+				u8g2.setCursor(5 + offset * 2, 9 + 9 * (i - 6));
+			else if (i < 12)
+				u8g2.setCursor(5 + offset * 3, 9 + 9 * (i - 9));
+			else if (i < 15)
+				u8g2.setCursor(5 + offset * 0, 15 + 9 * (i - 9));
+			else if (i < 18)
+				u8g2.setCursor(5 + offset * 1, 15 + 9 * (i - 12));
+			else if (i < 21)
+				u8g2.setCursor(5 + offset * 2, 15 + 9 * (i - 15));
+			else if (i < 24)
+				u8g2.setCursor(5 + offset * 3, 15 + 9 * (i - 18));
+			else if (i < 27)
+				u8g2.setCursor(5 + offset * 4, 15 + 9 * (i - 21));
+			if (doc["devices"][i]["found"])
+			{
+				if (i < 10)
+					u8g2.print(".0" + (String)i + ".");
+				else
+					u8g2.print("." + (String)i + ".");
+			}
+			else
+			{
+				if (i < 10)
+					u8g2.print("?0" + (String)i + "?");
+				else
+					u8g2.print("?" + (String)i + "?");
+			}
+		}
+		u8g2.setDrawColor(1);
+
+		u8g2.sendBuffer();
+	}
+	u8g2.setDrawColor(1);
+	u8g2.clearBuffer();
+	u8g2.sendBuffer();
+}
+//DynamicJsonDocument Framies(16384*2);
+String getValue(String data, char separator, int index)
+{
+	int found = 0;
+	int strIndex[] = {0, -1};
+	int maxIndex = data.length() - 1;
+
+	for (int i = 0; i <= maxIndex && found <= index; i++)
+	{
+		if (data.charAt(i) == separator || i == maxIndex)
+		{
+			found++;
+			strIndex[0] = strIndex[1] + 1;
+			strIndex[1] = (i == maxIndex) ? i + 1 : i;
+		}
+	}
+	return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+void Blinky(String name)
+{
+	u8g2.clearBuffer();
+	uint8_t color[5];
+	uint8_t pp[250 * 27 * sizeof(color)];
+	handleInput();
+	handleInput();
+	handleInput();
+	Serial.println("started");
+	//SdCard = SD.open("random.dat");
+	SdCard = SD.open(name);
+	Serial.println("open:" + name);
+	//const char* cstr = name;
+	//std::string pps(cstr);
+	int frames = getValue(name, '_', 0).toInt();
+	Serial.println("Created");
+	SdCard.read((uint8_t *)&pp, sizeof(pp));
+	Serial.print("read pp:");
+	Serial.println(SdCard.readString());
+	float biges = 0;
+	int frame = 0;
+	int frame2 = 0;
+	bool runing = true;
+	while (runing)
+		for (int f = 0; f < frames; f++)
+		{
+			u8g2.setFont(u8g2_font_amstrad_cpc_extended_8f);
+			for (int x = 0; x < 3; x++)
+				for (int y = 0; y < 9; y++)
+				{
+					int led = x * 9 + y;
+					u8g2.setCursor(y * 13 + 10, x * 13 + 20);
+					int fuR = pp[f * 27 * sizeof(color) + led * sizeof(color) + 0] * 2;
+					int fuG = pp[f * 27 * sizeof(color) + led * sizeof(color) + 1] * 2;
+					int fuB = pp[f * 27 * sizeof(color) + led * sizeof(color) + 2] * 2;
+					int coli = (fuR + fuG + fuB) / 3;
+					char myASCII[] = " .:-=+*#%@";
+					u8g2.print(myASCII[int(float(coli) / 256 * (sizeof(myASCII) / sizeof(*myASCII)))]);
+				}
+			u8g2.sendBuffer();
+			handleInput();
+			biges += 0.2;
+			if (frame > 255)
+			{
+				frame = 0;
+				frame2++;
+			}
+			uint8_t a = (sin(biges) + 1) / 2 * 255;
+			long startMilis = millis();
+			server.handleClient();
+
+			if (butonRn)
+			{
+				runing = false;
+				break;
+			}
+			for (int i = 0; i < 27; i++)
+			{
+				IPAddress pps;
+				pps.fromString(doc["devices"][i]["IP"].as<String>());
+
+				for (int j = 0; j < 16; j++)
+				{
+					UDP.beginPacket(pps, UDP_PORT);
+
+					//for (int jj = 0; jj < 16; jj++)
+					UDP.write(uint8_t(pp[f * 27 * sizeof(color) + i * sizeof(color) + 0] * 2));
+					UDP.write(uint8_t(pp[f * 27 * sizeof(color) + i * sizeof(color) + 1] * 2));
+					UDP.write(uint8_t(pp[f * 27 * sizeof(color) + i * sizeof(color) + 2] * 2));
+					UDP.write(frame);
+					UDP.write(frame2);
+					UDP.endPacket();
+				}
+
+				delay(1);
+			}
+
+			if ((millis() - startMilis) < 100)
+			{
+				delay(100 - (millis() - startMilis));
+			}
+		}
+
+	Blank();
+	Serial.println("Send done");
+	SdCard.close();
+	Serial.println("close");
+}
+int animSelect = 0;
+bool isAnim(const std::string &str)
+{
+	std::string suffix = ".dat";
+	bool result = false;
+	return (str.size() >= suffix.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0);
+	//return(true);
+}
+void Anims()
+{
+	File root = SD.open("/");
+	int AnimCount = 0;
+	int FileCount = 0;
+
+	while (true)
+	{
+		File entry = root.openNextFile();
+		if (!entry)
+		{
+			root.rewindDirectory();
+			break;
+		}
+		else if (isAnim(entry.fullName()))
+		{
+			AnimCount++;
+		}
+		entry.close();
+	}
+	while (true)
+	{
+		File entry = root.openNextFile();
+		if (!entry)
+		{
+			root.rewindDirectory();
+			break;
+		}
+		else
+		{
+			FileCount++;
+		}
+		entry.close();
+	}
+
+	String *AnimList = new String[AnimCount + 1];
+	int animAAAAA = 1;
+	for (int i = 0; i < FileCount; i++)
+	{
+		File entry = root.openNextFile();
+		if (isAnim(entry.fullName()))
+		{
+			AnimList[animAAAAA] = entry.name();
+			entry.close();
+			Serial.println(AnimList[animAAAAA]);
+			animAAAAA++;
+		}
+	}
+	AnimList[0] = "Exit";
+	AnimCount++;
+	while (true)
+	{
+		u8g2.clearBuffer();
+		server.handleClient();
+		handleInput();
+		if (UpRn)
+			animSelect++;
+		if (DownRn)
+			animSelect--;
+
+		if (animSelect < 0)
+			animSelect = AnimCount - 1;
+		if (animSelect > AnimCount - 1)
+			animSelect = 0;
+		u8g2.setFont(u8g2_font_t0_11b_tf);
+
+		for (int i = 0; i < AnimCount; i++)
+		{
+			int why = i - animSelect;
+			if (why >= 0 && why < 4)
+			{
+				u8g2.setCursor(10, 12 + why * 12);
+				if (animSelect == i)
+					u8g2.print("| " + AnimList[i]);
+				else
+					u8g2.print(AnimList[i]);
+			}
+		}
+		if (butonRn)
+		{
+			if (animSelect == 0)
+				break;
+			else
+				Blinky(AnimList[animSelect]);
+		}
+		u8g2.sendBuffer();
+	}
+}
+
+String menu[4] = {"Connected", "Anim", "Other2", "Other3"};
+int selection = 0;
+void loop()
+{
+	u8g2.clearBuffer();
+
+	server.handleClient();
+	handleInput();
+	if (UpRn)
+		selection++;
+	if (DownRn)
+		selection--;
+
+	if (selection < 0)
+		selection = 3;
+	if (selection > 3)
+		selection = 0;
+	u8g2.setFont(u8g2_font_t0_11b_tf);
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		u8g2.setCursor(10, 12 + i * 12);
+		if (selection == i)
+			u8g2.print("| " + menu[i]);
+		else
+			u8g2.print(menu[i]);
+	}
+	if (butonRn)
+	{
+		if (selection == 0)
+			ConnectedOnes();
+		if (selection == 1)
+			Anims();
+	}
+	u8g2.sendBuffer();
+	delay(50);
+}
+void HandleMyData()
+{
+	if (server.arg("Mac") != "" && server.arg("IP") != "" && server.arg("ID") != "")
+	{
+		SdCard = SD.open("modules.json", 2);
+		Serial.println(atoi(server.arg("ID").c_str()));
+		for (int i = 0; i < doc["devices"].size(); i++)
+		{
+			if (int(doc["devices"][i]["id"]) == atoi(server.arg("ID").c_str()))
+			{
+				server.send(200, "application/json", "{\"found\":true\"}");
+
+				doc["devices"][i]["IP"] = server.arg("IP");
+				doc["devices"][i]["Mac"] = server.arg("Mac");
+				doc["devices"][i]["found"] = true;
+				break;
+			}
+		}
+		Serial.println(doc.as<String>());
+		server.send(200, "application/json", "{ \"found\":false}");
+		serializeJson(doc, SdCard);
+		SdCard.close();
+	}
+}
